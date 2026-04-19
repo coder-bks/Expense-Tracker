196a618ae5c205c564a8749056025237c585a9ff
balkrishna sawant <balkrishnaraghunathsawant@gmail.com>
2026-04-19 18:13:09 +0530

    updated nix and requirements.txt for deployment

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session
 from app.core.config import settings
 
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=False)
 
 
 
